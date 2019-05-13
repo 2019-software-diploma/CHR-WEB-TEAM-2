02875c7051bf473be94dcee60af80a28aaa95345
Shuaiqiang Yin <jasonyin0305@hotmail.com>
2019-05-13 12:01:51 +1000

    Updated Make an Appointment page (JS/CSS/HTML)
    Signed-off-by: Shuaiqiang Yin <jasonyin0305@hotmail.com>

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,14 @@
 function flip() {
     var btn = document.querySelector("#Login").classList.toggle("flip");
 }
+
+function telephoneCheck(str) {
+    var patt = new RegExp(/^\+?1?\s*?\(?\d{3}(?:\)|[-|\s])?\s*?\d{3}[-|\s]?\d{4}$/);
+    return patt.test(str);
+}
+
+function formInputCheck(obj) {
+    $("#" + obj.id).removeClass("is-valid");
+    $("#" + obj.id).removeClass("is-invalid");
+    telephoneCheck(obj.value) ? $("#" + obj.id).addClass("is-valid") : $("#" + obj.id).addClass("is-invalid");
+}
