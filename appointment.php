6d0408bf43bca29820d0fec85b3b03591b93a801
Shuaiqiang Yin <jasonyin0305@hotmail.com>
2019-05-13 14:41:48 +1000

    Delete html file and Make a PHP file
    Signed-off-by: Shuaiqiang Yin <jasonyin0305@hotmail.com>

---
+++ appointment.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Shuaiqiang Yin
+ * Date: 2019-05-13
+ * Time: 13:30
+ */
+
+session_start();
+$_SESSION;
+require_once "theme/default/header.php";
+/*Main Start*/
+?>
+<main>
+    <div class="container maa_main">
+        <div class="maa_main_title">
+            <h2>Make an appointment</h2>
+            <p>Please fill in your personal information and the reason for your appointment below:</p>
+        </div>
+        <form action="#" method="post"><!-- TODO action page -->
+            <div class="form-row">
+                <div class="form-group col-md-4">
+                    <label for="first_name">First Name</label>
+                    <input type="text" class="form-control" id="first_name" placeholder="First Name"
+                           onfocusout="formInputCheck(this);" required>
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="last_name">Last Name</label>
+                    <input type="text" class="form-control" id="last_name" placeholder="Last Name"
+                           onfocusout="formInputCheck(this);" required>
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="phone_number">Phone Number</label>
+                    <input type="text" class="form-control" id="phone_number" placeholder="Phone Number"
+                           onkeypress="return event.charCode >= 48 && event.charCode <= 57 && value.length < 10;"
+                           onfocusout="formInputCheck(this);" required>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="address">Address</label>
+                <input type="text" class="form-control" id="address" placeholder="1234 Main St, ACT, 2614"
+                       onfocusout="formInputCheck(this);" required>
+            </div>
+            <div class="form-group">
+                <label for="reason">Brief of reason:</label>
+                <textarea class="form-control" rows="5" id="reason" onfocusout="formInputCheck(this);" required
+                          maxlength="500"></textarea>
+                <span class="float-right tag tag-default" id="count_message"></span>
+            </div>
+            <p class="inForm-text">Who would you like to make an appointment with and what time?</p>
+            <div class="form-row">
+                <div class="form-group col-md-4">
+                    <label for="staff">Staff</label>
+                    <select id="staff" class="form-control" required>
+                        <option selected>Test Person 1</option>
+                        <option>Test Person 2</option>
+                        <option>Test Person 3</option>
+                        <option>Test Person 4</option>
+                        <option>Test Person 5</option>
+                        <option>Test Person 6</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="date">Date</label>
+                    <input id="date" class="form-control" type="date" placeholder="DD/MM/YYYY"
+                           onfocusout="formInputCheck(this);" required>
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="time">Time</label>
+                    <input id="time" class="form-control" type="time" placeholder="HH:MM AM"
+                           onfocusout="formInputCheck(this);" required>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-3">
+                    <input class="form-control" type="reset" value="Reset">
+                </div>
+                <div class="form-group col-md-3"></div>
+                <div class="form-group col-md-3"></div>
+                <div class="form-group col-md-3">
+                    <input class="form-control" type="submit" value="Submit">
+                </div>
+            </div>
+        </form>
+    </div>
+</main>
+<?php
+/*Main End*/
+require_once "theme/default/footer.php";
+?>
--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,17 @@
+windowsLoad();
+
+function windowsLoad() {
+    var text_max = 500;
+    $('#count_message').html(text_max + ' / ' + text_max);
+    $('#reason').keyup(function () {
+        var text_length = $('#reason').val().length;
+        var text_remaining = text_max - text_length;
+        $('#count_message').html(text_remaining + ' / ' + text_max);
+    });
+    $('#reason').keypress(function () {
+        return $('#reason').val().length < 500;
+    });
+}
 function flip() {
     var btn = document.querySelector("#Login").classList.toggle("flip");
 }
@@ -10,5 +24,15 @@ function telephoneCheck(str) {
 function formInputCheck(obj) {
     $("#" + obj.id).removeClass("is-valid");
     $("#" + obj.id).removeClass("is-invalid");
-    telephoneCheck(obj.value) ? $("#" + obj.id).addClass("is-valid") : $("#" + obj.id).addClass("is-invalid");
+    if (obj.value != "") {
+        $("#" + obj.id).addClass("is-valid");
+    } else {
+        $("#" + obj.id).addClass("is-invalid");
+    }
+    if (obj.id == "phone_number") {
+        $("#" + obj.id).removeClass("is-valid");
+        $("#" + obj.id).removeClass("is-invalid");
+        telephoneCheck(obj.value) ? $("#" + obj.id).addClass("is-valid") : $("#" + obj.id).addClass("is-invalid");
+
+    }
 }
